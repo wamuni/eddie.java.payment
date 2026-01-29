9255303dbe9e708aee68f28e959d1666f211737b
wamuni <boshi.pan@outlook.com>
2026-01-29 23:36:53 +0000

    payment api, database set up

---
+++ src/main/resources/db/migration/V3__payments.sql
@@ -0,0 +1,24 @@
+create table payments (
+	id bigserial primary key,
+
+	order_id bigint not null refreneces orders(id),
+
+	idempotency_key varchar(255) not null,
+
+	status varchar(32) not null,
+	-- PENDING | SUCCEEDED | FAILED
+
+	amount numeric(19, 4) not null,
+
+	currency char(3) not null,
+
+	method varchar(32) not null,
+	-- CARD (for MVP)
+
+	failure_reason varchar(255),
+
+	created_at timestamptz not null default now(),
+	updated_at timestamptz not null default now(),
+
+	constraint uq_payment_order_idempotency unique (order_id, idempotency_key)
+);
